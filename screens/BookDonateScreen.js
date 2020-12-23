cfd42609eaf8da48fd5bce081018be33238f91da
srushteeM <59560681+srushteeM@users.noreply.github.com>
2020-12-23 14:08:38 +0530

    Add files via upload

---
+++ screens/BookDonateScreen.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, FlatList,TouchableOpacity } from 'react-native';
+import { ListItem } from 'react-native-elements'
+import firebase from 'firebase';
+import db from '../config'
+import MyHeader from '../components/MyHeader';
+
+export default class BookDonateScreen extends Component{
+ 
+  render(){
+    return(
+      <View style={{flex:1}}>
+        <MyHeader title="Donate Books"/>
+       
+        </View>
+    
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  subContainer:{
+    flex:1,
+    fontSize: 20,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button:{
+    width:100,
+    height:30,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8
+     }
+  }
+})
